3c1397bf644bf5746bcc8933ed40b074f92f6272
Bastian Ilso <contact@bastianilso.com>
2020-01-10 10:20:38 +0100

    OrangeBehavior: Dont play Animation on Start()
    Old remnant from testing the animation controller.

--- Assets/OrangeBehavior.cs
+++ Assets/OrangeBehavior.cs
@@ -11,7 +11,6 @@ public class OrangeBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        anim.Play("Squeezed");
     }
 
     // Update is called once per frame
